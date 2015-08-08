927fb6a2d314b4ac041d6f688d3fe5de6631c2f7
Andrew Jones <andyjones.ed@gmail.com>
2015-08-08 21:31:49 +0100

    Improves template

--- templater.py
+++ templater.py
@@ -3,7 +3,7 @@ from jinja2 import Environment, FileSystemLoader
 
 WEEKS_PER_MONTH = 365/12./7
 
-def get_filtered_listings():
+def get_listings():
     listings = json.load(open('resources/listings.json', 'r'))
     results = []
     for listing in listings.values():
@@ -11,12 +11,14 @@ def get_filtered_listings():
             listing['price'] = int(WEEKS_PER_MONTH*int(listing['price']))
             results.append(listing)
 
+    results = sorted(results, key=lambda r: r['last_published_date'], reverse=True)
+
     return results
 
 def get_rendered_page():
     env = Environment(loader=FileSystemLoader('.'))
     template = env.get_template('index_template.html')
-    rendered = template.render(listings=get_filtered_listings())
+    rendered = template.render(listings=get_listings())
     return rendered
 
 with open('index.html', 'w+') as f:
