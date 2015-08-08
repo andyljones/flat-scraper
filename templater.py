92faf4ea69eb8761dcb213c8a23ba4d60ce1f48f
Andrew Jones <andyjones.ed@gmail.com>
2015-08-08 19:06:10 +0100

    Updates templater to use loop

--- templater.py
+++ templater.py
@@ -1,7 +1,11 @@
+import json
 from jinja2 import Environment, FileSystemLoader
+
+listings = json.load(open('resources/listings.json', 'r'))
+
 env = Environment(loader=FileSystemLoader('resources'))
 template = env.get_template('index_template.html')
-rendered = template.render(test='hello')
+rendered = template.render(listings=listings)
 
 with open('resources/index.html', 'w+') as f:
     f.write(rendered)
