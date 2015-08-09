6be17d12ddf1f1c6db2a13ad254602d53a8490ed
Andrew Jones <andyjones.ed@gmail.com>
2015-08-09 09:05:49 +0100

    Fixes bugs with uploader script

--- github_uploader.py
+++ github_uploader.py
@@ -1,19 +1,24 @@
 import datetime
+import sys
+import os
 
 from git import Repo
 from templater import generate_index
+from listing_scraper import scrape_listings_and_images
 
 def update_website():
     repo = Repo('.')
     current_branch = repo.active_branch
 
+    print('Scraping listings...')
+    scrape_listings_and_images()
+
     try:
+        repo.heads['gh-pages'].checkout()
         print('Generating index.html...')
-        repo.heads['master'].checkout()
         generate_index()
 
         print('Adding to branch gh-pages...')
-        repo.heads['gh-pages'].checkout()
         repo.index.add(['index.html', 'index.css', 'photos'], force=True)
         repo.index.commit(str.format('Updated at {}', str(datetime.datetime.now())))
 
@@ -25,4 +30,5 @@ def update_website():
     finally:
         current_branch.checkout()
 
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 1)
 update_website()
